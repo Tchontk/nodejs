2289e2cf57214c55e15a77292e41abbc007993f0
Pierrick <pleduc@jpquery.fr>
2018-01-31 12:53:47 +0100

    Modules

--- 03 - Node.js Fundamentals (Note App)/notes-node/app.js
+++ 03 - Node.js Fundamentals (Note App)/notes-node/app.js
@@ -4,15 +4,19 @@ const fs = require('fs'); // https://nodejs.org/api/fs.html
 const os = require('os'); // https://nodejs.org/api/os.html
 var user = os.userInfo();
 const notes = require('./notes.js');
+const _ = require('lodash')
 
 var res = notes.addNote()
 var add = notes.add
 
+var filteredArray = _.uniq(['A', 'B', 4, 'A', 5, 'C', 4])
 console.log(notes.age);
 console.log(res);
+console.log(filteredArray);
 
 console.log('Result:', add(9, -2));
 
+console.log(_.camelCase('EezjrlAE$Akmezkr-zzer'))
 
 fs.appendFile('greetings.txt', `Hello ${user.username}, Age : ${notes.age}`, (err, data) => {
   if (err) {
