a3be4c5883a8517da789608fe51f413009a79e84
tchontk <tchontk@gmail.com>
2018-02-03 23:02:26 +0100

    geocode module

--- 03 - Asynchronous Node.js (Weather App)/weather-app/app.js
+++ 03 - Asynchronous Node.js (Weather App)/weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request')
 const yargs = require('yargs')
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
     // .command('search', 'Search address', {
@@ -23,29 +23,4 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.a)
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    // body.results.forEach(result => {
-    //     console.log(JSON.stringify(result.geometry, undefined,
-    //         2));
-    // });
-    if (error) {
-        // error ==> host not found
-        console.log('Unable to connect')
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Enable to find that address');
-    } else if (body.status === 200) {
-        // console.log(JSON.stringify(response, undefined, 2));
-        console.log(`Address : ${body.results[0].formatted_address}`);
-        console.log(
-            `Latitude : ${body.results[0].geometry.location.lat}`);
-        console.log(
-            `Longitude : ${body.results[0].geometry.location.lng}`);
-    } else {
-        console.log(`Error : ${response.statusCode}`);
-    }
-})
+geocode.geocodeAddress(argv.a)
