bf8d06c7461f56c319ddf3c1241451916f867991
tchontk <tchontk@gmail.com>
2018-02-03 22:46:33 +0100

    Error

--- 03 - Asynchronous Node.js (Weather App)/weather-app/app.js
+++ 03 - Asynchronous Node.js (Weather App)/weather-app/app.js
@@ -29,15 +29,23 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    // error ==> host not found
-    // console.log(JSON.stringify(response, undefined, 2));
     // body.results.forEach(result => {
     //     console.log(JSON.stringify(result.geometry, undefined,
     //         2));
     // });
-    console.log(`Address : ${body.results[0].formatted_address}`);
-    console.log(
-        `Latitude : ${body.results[0].geometry.location.lat}`);
-    console.log(
-        `Longitude : ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        // error ==> host not found
+        console.log('Unable to connect')
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Enable to find that address');
+    } else if (body.status === 200) {
+        // console.log(JSON.stringify(response, undefined, 2));
+        console.log(`Address : ${body.results[0].formatted_address}`);
+        console.log(
+            `Latitude : ${body.results[0].geometry.location.lat}`);
+        console.log(
+            `Longitude : ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log(`Error : ${response.statusCode}`);
+    }
 })
