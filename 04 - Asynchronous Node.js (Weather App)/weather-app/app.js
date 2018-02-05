a30b230e8c5c6ba9e5b20aae55ec915f848f48ee
tchontk <tchontk@gmail.com>
2018-02-05 23:20:36 +0100

    Weather - Without promise :(

--- 04 - Asynchronous Node.js (Weather App)/weather-app/app.js
+++ 04 - Asynchronous Node.js (Weather App)/weather-app/app.js
@@ -1,25 +1,38 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs // eslint-disable-line
-  .options({
-    address: {
-      a: {
-        describe: 'adress',
-        demand: true,
-        alias: 'a',
-        string: true,
-      },
-    },
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+    .options({
+        address: {
+            a: {
+                describe: 'adress',
+                demand: true,
+                alias: 'a',
+                string: true,
+            },
+        },
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
 
 geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
+    if (errorMessage) {
+        console.log(errorMessage);
+        // return undefined;
+    }
+    console.log(results.address);
+    weather.getweather(results.latitude, results.longitude, (errorMessage, results) => {
+        if (errorMessage) {
+            console.log(errorMessage);
+            // return undefined;
+        }
+        console.log(`Currently ${results.temperature}. It feels like ${results.apparentTemperature}`);
+        // console.log(JSON.stringify(results, undefined, 2));
+        // return results;
+    });
+    // console.log(JSON.stringify(results, undefined, 2));
+    // return results;
 });
--- 04 - Asynchronous Node.js (Weather App)/weather-app/geocode/geocode.js
+++ 04 - Asynchronous Node.js (Weather App)/weather-app/geocode/geocode.js
@@ -1,29 +1,25 @@
 const request = require('request');
 
 const geocodeAddress = (address, callback) => {
-  const encodedAddress = encodeURIComponent(address);
-  request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true,
-  }, (error, response, body) => {
-    if (error) {
-      callback('Unable to connect');
-    } else if (body.status !== 'OK') {
-      callback(`Error : '${body.status}`);
-    } else if (body.status === 'OK') {
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        Latitude: body.results[0].geometry.location,
-        Longitude: body.results[0].geometry.location,
-      });
-    } else {
-      callback(`Error : ${response.statusCode}`);
-    }
-  });
+    const encodedAddress = encodeURIComponent(address);
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        json: true,
+    }, (error, response, body) => {
+        if (error) {
+            callback('Unable to connect');
+        } else if (body.status !== 'OK') {
+            callback(`Error : '${body.status}`);
+        } else if (body.status === 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng,
+            });
+        } else {
+            callback(`Error : ${response.statusCode}`);
+        }
+    });
 };
-
-// module.exports = {
-//     geocodeAddress
-// };
 
 module.exports.geocodeAddress = geocodeAddress;
