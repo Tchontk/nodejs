7cc5ed7d7b3c9ebf2b363d93e2474f770d461956
tchontk <tchontk@gmail.com>
2018-02-06 23:27:11 +0100

    template hbs

--- 05 - Web Servers and Application Deployment/node-web-server/server.js
+++ 05 - Web Servers and Application Deployment/node-web-server/server.js
@@ -1,16 +1,37 @@
-const express = require('express')
+const express = require('express');
+const hbs = require('hbs');
 
 var app = express()
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express !</h1>')
-  res.send({
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello ScReAm It',
     name: 'P',
-    Likes: ['Running', 'Biking']
+    hobbies: ['Running', 'Biking']
   })
+  // res.send({
+  //   name: 'P',
+  //   Likes: ['Running', 'Biking']
+  // })
 })
 app.get('/about', (req, res) => {
-  res.send('<h1>About Page !</h1>')
+  // res.send('<h1>About Page !</h1>')
+  res.render('about.hbs', {
+    pageTitle: 'About Page !',
+  })
 })
 
 app.get('/error', (req, res) => {
@@ -19,4 +40,6 @@ app.get('/error', (req, res) => {
   })
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server is listen on port 3000');
+})
