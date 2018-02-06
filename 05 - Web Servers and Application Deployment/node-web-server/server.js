dd30a39dff03982b015d019b1478442270577902
tchontk <tchontk@gmail.com>
2018-02-06 22:25:46 +0100

    My first  Express :')

--- 05 - Web Servers and Application Deployment/node-web-server/server.js
+++ 05 - Web Servers and Application Deployment/node-web-server/server.js
@@ -3,5 +3,20 @@ const express = require('express')
 var app = express()
 
 app.get('/', (req, res) => {
-
+  // res.send('<h1>Hello Express !</h1>')
+  res.send({
+    name: 'P',
+    Likes: ['Running', 'Biking']
+  })
 })
+app.get('/about', (req, res) => {
+  res.send('<h1>About Page !</h1>')
+})
+
+app.get('/error', (req, res) => {
+  res.send({
+    errorMessage: 'Error Message'
+  })
+})
+
+app.listen(3000)
