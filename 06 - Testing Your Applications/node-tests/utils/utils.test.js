d152380b0821ee881c45ad10c7c53b2c8da8cae9
Pierrick <>
2018-02-08 13:22:30 +0100

    Mocha

---
+++ 06 - Testing Your Applications/node-tests/utils/utils.test.js
@@ -0,0 +1,14 @@
+const utils = require('./utils')
+it('Should add two numbers', () => {
+  res = utils.add(33, 11)
+  if (res !== 44) {
+    throw new Error(`Exprected 44, but got ${res}`)
+  }
+});
+
+it('Should square a number', () => {
+  res = utils.square(3)
+  if (res !== 9) {
+    throw new Error(`Exprected 9, but got ${res}`)
+  }
+});
