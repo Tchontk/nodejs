29e48cefae560f1125975ff0247dba06a35da3f2
Pierrick <tchontk@gmail.com>
2018-02-19 21:08:19 +0100

    beautify

--- 08 - Security and Authentication/node-todo-api/playground/mongodb-find.js
+++ 08 - Security and Authentication/node-todo-api/playground/mongodb-find.js
@@ -1,5 +1,5 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -12,7 +12,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // }).toArray().then((docs) => {
   //   console.log('Todos');
   //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
+  // }, (err) => { 
   //   console.log('Unable to fetch todos', err);
   // });
 
@@ -22,7 +22,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log('Unable to fetch todos', err);
   // });
 
-  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+  db.collection('Users').find({ name: 'Andrew' }).toArray().then((docs) => {
     console.log(JSON.stringify(docs, undefined, 2));
   });
 
--- 08 - Security and Authentication/node-todo-api/playground/mongodb-update.js
+++ 08 - Security and Authentication/node-todo-api/playground/mongodb-update.js
@@ -1,5 +1,5 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -11,7 +11,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
   // }, {
   //   $set: {
-  //     completed: true
+  //     completed: true 
   //   }
   // }, {
   //   returnOriginal: false
