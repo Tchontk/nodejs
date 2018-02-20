70a15bebb2b798f7372ba9b2dfa1a717113bc5d3
Pierrick <>
2018-02-20 13:54:01 +0100

    seed

---
+++ 08 - Security and Authentication/node-todo-api/server/tests/seed/seed.js
@@ -0,0 +1,18 @@
+const { ObjectID } = require('mongodb');
+const { Todo } = require('./../../models/todo');
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
+}];
+
+const populate = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+}
