dc8be93f4c19d8275bc3214acc425cf041a7824c
Pierrick <>
2018-02-23 15:17:07 +0100

    GET /todos/:id - début modification

--- 08 - Security and Authentication/node-todo-api/server/server.js
+++ 08 - Security and Authentication/node-todo-api/server/server.js
@@ -40,7 +40,7 @@ app.get('/todos', authenticate, (req, res) => {
     });
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
@@ -48,7 +48,7 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo
-    .findById(id)
+    .findOne({ _id: id, _creator: req.user._id })
     .then((todo) => {
       if (!todo) {
         return res.status(404).send();
