45f02f021c4f3297f986ca44fdeccec50a458844
Pierrick <>
2018-03-01 13:43:41 +0100

    Async

--- 10 - AsyncAwait/async-await/currency-convert.js
+++ 10 - AsyncAwait/async-await/currency-convert.js
@@ -2,20 +2,94 @@
 // 23 USD is worth 28 CAD. You can spend these in the following countries:
 const axios = require('axios');
 
-const base = 'USD'
-const search = 'CAD'
-const fixerUrl = `https://api.fixer.io/latest?base=`
-const restCountriesUrl = `https://restcountries.eu/rest/v2/currency/`
+const from = 'EUR'
+const to = 'CAD'
+const amount = 100
 
-const fixerUrlSearch = fixerUrl + base
-const restCountriesUrlSearch = restCountriesUrl + base
-console.log(restCountriesUrlSearch);
-console.log(fixerUrl);
+// const getExchangesRate = (from, to) => {
+//   return axios
+//     .get(`https://api.fixer.io/latest?base=${from}`)
+//     .then((response) => {
+//       return response.data.rates[to]
+//     })
+// }
 
-const getRates = async () => {
-  const rates = await axios.get(fixerUrlSearch)
-  const restCountries = await axios.get(restCountriesUrlSearch)
-  console.log(rates.data.rates);
-  console.log(restCountries.data[0].name);
+// const getCountries = (currencyCode) => {
+//   return axios
+//     .get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+//     .then((response) => {
+//       // return response.data.map((country) => {
+//       //   return country.name
+//       // })
+//       return response.data.map((country) => country.name)
+//     })
+// }
+
+// const convertCurrencyPromise = (from, to, amount) => {
+//   let countries
+//   return getCountries(to)
+//     .then((countriesTemp) => {
+//       countries = countriesTemp
+//       return getExchangesRate(from, to)
+//     })
+//     .then((rate) => {
+//       const exchangeAmount = amount * rate
+//       return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in the following countries : ${countries.join(', ')} `
+//       // console.log(countries);
+//       // console.log(rate);
+//     })
+// }
+
+// getExchangesRateA(from, to).then((rate) => {
+//   console.log(rate); 
+// }) 
+
+// getCountriesA(to).then((countries) => {
+//   console.log(countries);
+// })
+
+// convertCurrencyPromise(from, to, amount)
+//   .then((status) => {
+//     console.log('convertCurrencyPromise', status);
+//   })
+
+const getExchangesRate = async (from, to) => {
+  try {
+    const response = await axios.get(`https://api.fixer.io/latest?base=${from}`)
+    const rate = response.data.rates[to]
+    if (rate) {
+      return rate
+    } else {
+      throw new Error()
+    }
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}  `)
+  }
 }
-getRates()
+
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+    return response.data.map((country) => country.name)
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode} `)
+  }
+}
+
+const convertCurrencyAsync = async (from, to, amount) => {
+  const rate = await getExchangesRate(from, to)
+  const countries = await getCountries(to)
+  const exchangeAmount = amount * rate
+  return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in the following countries : ${countries.join(', ')} `
+  // const rate = await axios.get(`https://api.fixer.io/latest?base=${from}`)
+  // const countries = await axios.get(`https://restcountries.eu/rest/v2/currency/${to}`)
+  // const exchangeAmount = amount * rate.data.rates[to]
+  // return `${amount} ${from} is worth ${exchangeAmount} ${to}. You can spend in the following countries : ${countries.data.map((country) => country.name).join(', ')} `
+}
+
+convertCurrencyAsync(from, to, amount)
+  .then((status) => {
+    console.log('convertCurrencyAsync', status);
+  }).catch((e) => {
+    console.log(e.message);
+  })
